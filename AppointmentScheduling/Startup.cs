afbc74c9319673fa7eea9473b7a1645e49066e51
Abhijit Mondal Abhi <abhi.m.abhijit@gmail.com>
2021-06-15 02:07:21 +0200

    Sign Up and Sign In partial view for navbar

--- AppointmentScheduling/Startup.cs
+++ AppointmentScheduling/Startup.cs
@@ -9,6 +9,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AppointmentScheduling.Data;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace AppointmentScheduling
@@ -28,6 +29,7 @@ namespace AppointmentScheduling
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllersWithViews();
+            services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
